57323b97ccdfe03425db9458b707d2e58851d6b5
gokul8983 <56394676+gokul8983@users.noreply.github.com>
2022-11-17 13:42:17 +0530

    Add files via upload
    This application helps to colleges to provide answer to students queries related exam, scholarship, placement etc.

---
+++ Default.aspx.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Drawing;
+
+public partial class _Default : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (!Page.IsPostBack)
+        {
+            txtUsername.Focus();
+            
+        }
+    }
+
+    protected void btnLogin_Click(object sender, EventArgs e)
+    {
+
+        try
+        {
+            Login login = new Login();
+            DataTable dt = login.CheckAdminUser(txtUsername.Text.Trim(), txtPassword.Text.Trim());
+            if (dt.Rows.Count > 0)
+            {
+                Session["LoginId"] = dt.Rows[0]["Id"].ToString();
+                Session["Role"] = "Admin";
+                Session["UserName"] = dt.Rows[0]["UserName"].ToString();
+                Response.Redirect("~/Common/DashBoard.aspx");
+            }
+            else
+            {
+                lblmsg.Text = "Incorrect Credential. Please try again!";
+                lblmsg.ForeColor = Color.Red;
+            }
+        }
+        catch (Exception ex)
+        {
+            lblmsg.Text = ex.Message;
+            lblmsg.ForeColor = Color.Red;
+        }
+    }
+
+    protected void btnStudentLogin_Click(object sender, EventArgs e)
+    {
+        Response.Redirect("~/StudentLogin.aspx");
+        Response.Redirect("~/Default.aspx");
+    }
+}
---
+++ StudentLogin.aspx.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Data;
+using System.Drawing;
+using System.Web.UI;
+
+public partial class _Default : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (!Page.IsPostBack)
+        {
+            txtUsername.Focus();
+        }
+    }
+
+    protected void btnLogin_Click(object sender, EventArgs e)
+    {
+        try
+        {
+            Login login = new Login();
+            DataTable dt = login.CheckStudentUser(txtUsername.Text.Trim(), txtPassword.Text.Trim());
+            if (dt.Rows.Count > 0)
+            {
+                Session["LoginId"] = dt.Rows[0]["Id"].ToString();
+                Session["UserName"] = dt.Rows[0]["StudentName"].ToString();
+                Session["Role"] = "Student";
+                Response.Redirect("~/Common/DashBoard.aspx");
+            }
+            else
+            {
+                lblmsg.Text = "Incorrect Credential. Please try again!";
+                lblmsg.ForeColor = Color.Red;
+            }
+        }
+        catch (Exception ex)
+        {
+            lblmsg.Text = ex.Message;
+            lblmsg.ForeColor = Color.Red;
+        }
+    }
+
+
+
+    protected void btnAdminLogin_Click(object sender, EventArgs e)
+    {
+        Response.Redirect("~/Default.aspx");
+    }
+
+    protected void btnStudentRegistration_Click(object sender, EventArgs e)
+    {
+        Response.Redirect("~/Student/StudentRegistration.aspx");
+    }
+}
